9d8a27dc73c9167dd2a061fb34e873c8c004cf28
Fateme <fzare22@gmail.com>
2020-09-30 01:29:56 +0330

    Change template

--- bamzi/views.py
+++ bamzi/views.py
@@ -102,31 +102,32 @@ def my_shares(request, username):
         #                 'pending_drugs_count': pending_drugs_count(request.user.hospital)}
         if request.method == 'POST':
             return JsonResponse(context_dict)
-        return render(request, 'bamzi/shares.html', context_dict)
+        return render(request, 'bamzi/index.html', context_dict)
     else:
-        return render(request, 'bamzi/shares.html', {'access': access})
+        return render(request, 'bamzi/index.html', {'access': access})
 
 # def my_share(request, username):
 #     return render(request, 'bamzi/index.html', {'share_data': {}})
 
 
 def update_share_data(request):
-    tn = TextNormalizer(TextNormalizerConfig.NORMAL_NAME_CONFIG)
-    tse_response = requests.get('http://www.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0')
-    if tse_response.status_code == 200:
-        tse_data = tse_response.text.split(';')
-        tse_data = list(map(lambda x: x.split(','), tse_data))
-        tse_data = list(filter(lambda x: not hasNumbers(x[2]) and len(x)==23, tse_data))
-        for share_data in tse_data:
-            share = Share.objects.filter(tse_id=share_data[0]).first()
-            if not share:
-                share = Share.objects.create(tse_id=share_data[0])
-                share.symbol_name = tn.normalize_text(share_data[2])
-                share.company_name = tn.normalize_text(share_data[3])
+    share_data = {}
+    # tn = TextNormalizer(TextNormalizerConfig.NORMAL_NAME_CONFIG)
+    # tse_response = requests.get('http://www.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0')
+    # if tse_response.status_code == 200:
+    #     tse_data = tse_response.text.split(';')
+    #     tse_data = list(map(lambda x: x.split(','), tse_data))
+    #     tse_data = list(filter(lambda x: not hasNumbers(x[2]) and len(x)==23, tse_data))
+    #     for share_data in tse_data:
+    #         share = Share.objects.filter(tse_id=share_data[0]).first()
+    #         if not share:
+    #             share = Share.objects.create(tse_id=share_data[0])
+    #             share.symbol_name = tn.normalize_text(share_data[2])
+    #             share.company_name = tn.normalize_text(share_data[3])
             
-            share.last_price = share_data[7]
-            share.final_price = share_data[6]
-            share.eps = 0 if not share_data[14] else int(share_data[14])
-            share.save()
+    #         share.last_price = share_data[7]
+    #         share.final_price = share_data[6]
+    #         share.eps = 0 if not share_data[14] else int(share_data[14])
+    #         share.save()
 
     return render(request, 'bamzi/index.html', {'share_data': share_data})
