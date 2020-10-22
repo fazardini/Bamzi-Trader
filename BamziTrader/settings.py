ac62f4038c9f5cf29f37679b253873ce60ee7f66
Fateme <fzare22@gmail.com>
2020-10-23 01:21:12 +0330

    Add Bamzi logo

--- BamziTrader/settings.py
+++ BamziTrader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#g9%vc(h9u^$3as-owlw)w^&1cq-d=@qil__))6w6n3*5m*0b1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bd41a2612d40.ngrok.io', '127.0.0.1', '192.168.1.9', '0.0.0.0']
+ALLOWED_HOSTS = ['7f742a06a647.ngrok.io', '127.0.0.1', '192.168.1.9', '0.0.0.0']
 
 
 # Application definition
